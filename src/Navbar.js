cda861dbc671d29724f3ae45881a3cb4902fa3cc
Aero 3D <x2000alexander@hotmail.com>
2025-02-08 23:21:30 +0100

    Adding new styles to Navbar and pages

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,13 +1,18 @@
 import './App.css';
-import { Link } from "react-router-dom"
+import { NavLink } from "react-router-dom"
 
 const Navbar =()=>{
-      return (
-            <div className="App-navbar">
-                  <Link className="App-link" to="/">Home</Link>
-                  <Link className="App-link" to="/about">About</Link>
-                  <Link className="App-link" to="/careers">Careers</Link>
-            </div>
-      )
+    return (
+        <div className="App-navbar">
+            {/* From v6 onwards of react-router replace activeClassName with className and use .isActive to toggle style.
+                <NavLink exact activeClassName="active" className="navbar-link" to="/">Home</NavLink>
+                <NavLink exact activeClassName="active" className="navbar-link" to="/about">About</NavLink>
+                <NavLink exact activeClassName="active" className="navbar-link" to="/careers">Careers</NavLink>
+            */}
+            <NavLink className={({ isActive }) => isActive? "navbar-link active": 'navbar-link'} to="/">Home</NavLink>
+            <NavLink className={({ isActive }) => isActive? "navbar-link active": 'navbar-link'} to="/about">About</NavLink>
+            <NavLink className={({ isActive }) => isActive? "navbar-link active": 'navbar-link'} to="/careers">Careers</NavLink>
+        </div>
+    )
 }
 export default Navbar;
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,11 +1,22 @@
+import logo from '../logo.svg';
 import '../App.css';
 
 function Home() {
     return (
-      <div className="App">
-          <h1>Home page</h1>
-      </div>
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+        </div>
     );
-  }
+}
   
-  export default Home;
+export default Home;
